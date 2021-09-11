0bade296558c211289e9b05b1eae4d09b4396d55
nookesh <nookeshkarri7@gmail.com>
2021-09-11 16:19:51 +0530

    added form method

--- mysite/views.py
+++ mysite/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(response):
-    return render(response,"index.html")
+    a={"name":"nookesh"}
+    return render(response,"index.html",a)
+
+def converttoupper(response):
+    a=(response.GET.get("textdata"))
+    b={"name":a}
+    return render(response,"index.html",b)
